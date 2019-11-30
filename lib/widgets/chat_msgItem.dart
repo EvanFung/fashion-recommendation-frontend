b59001a668efb0a47590b403fb149dc642cebb25
evanfung <evanfungv@gmail.com>
2019-11-30 16:55:00 +0800

    submission of FYP source code - 1

--- lib/widgets/chat_msgItem.dart
+++ lib/widgets/chat_msgItem.dart
@@ -12,20 +12,17 @@ class ChatMsgItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Container(
-          child: Text(
-            content,
-          ),
-          padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
-          width: 200.0,
-          decoration: BoxDecoration(
-              color: Colors.grey, borderRadius: BorderRadius.circular(8.0)),
-          margin: EdgeInsets.only(bottom: 20.0, right: 10.0),
-        )
-      ],
-      mainAxisAlignment: MainAxisAlignment.end,
-    );
+    return Row(children: <Widget>[
+      Container(
+        child: Text(
+          content,
+        ),
+        padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+        width: 200.0,
+        decoration: BoxDecoration(
+            color: Colors.grey, borderRadius: BorderRadius.circular(8.0)),
+        margin: EdgeInsets.only(bottom: 20.0, right: 10.0),
+      )
+    ], mainAxisAlignment: MainAxisAlignment.end);
   }
 }
